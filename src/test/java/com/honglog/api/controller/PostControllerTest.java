73765d556092052ba7cf0599da8077edfb1d599e
jihong <parkatrix@gmail.com>
2022-11-06 12:09:01 +0900

    Initial commit

---
+++ src/test/java/com/honglog/api/controller/PostControllerTest.java
@@ -0,0 +1,31 @@
+package com.honglog.api.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    @DisplayName(("/posts 요청 시 Hello World 를 출력한다"))
+    void test() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"))
+                .andDo(print());
+    }
+
+}
